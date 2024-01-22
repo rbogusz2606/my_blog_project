f7bb9cbb75a3459adce4698aa20f2eb81413f060
Rafal Bogusz <r.bogusz2606@gmail.com>
2024-01-22 14:54:19 +0100

    Dodanie projektu do bazy danych sql

--- project/project/settings.py
+++ project/project/settings.py
@@ -74,10 +74,20 @@ WSGI_APPLICATION = 'project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'railway',
+        'USER': 'postgres',
+        'PASSWORD': 'F6EBD*CbFg4ADAd*1Ca4C*1fcbE5dB35',
+        'HOST': 'monorail.proxy.rlwy.net',
+        'PORT': '23740',
     }
 }
 
